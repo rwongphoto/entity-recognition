28ec42229bb9e45fcdad508bf7d114eb6fabfc00
Richard Wong <46383420+rwongphoto@users.noreply.github.com>
2025-02-16 13:15:44 -0800

    Update cosine-similarity-tools.py

--- cosine-similarity-tools.py
+++ cosine-similarity-tools.py
@@ -65,6 +65,7 @@ def extract_text_from_url(url):
         # options.headless = False  # Remove or comment out headless mode
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36")
         driver = uc.Chrome(options=options)
         driver.get(url)
 
