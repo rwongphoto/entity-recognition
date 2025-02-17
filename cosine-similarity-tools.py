f83e9433a390daa735138cc83fb70bf7ce37ebb5
Richard Wong <46383420+rwongphoto@users.noreply.github.com>
2025-02-17 08:45:05 -0800

    Update cosine-similarity-tools.py

--- cosine-similarity-tools.py
+++ cosine-similarity-tools.py
@@ -15,7 +15,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException, WebDriverException
-
 import matplotlib.pyplot as plt
 import pandas as pd
 import spacy
