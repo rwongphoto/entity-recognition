2262209ce2261c39f819360f15b293ad836f3a84
Richard Wong <46383420+rwongphoto@users.noreply.github.com>
2025-03-23 12:15:13 -0700

    Update the-seo-consultant-ai.py

--- the-seo-consultant-ai.py
+++ the-seo-consultant-ai.py
@@ -6,7 +6,6 @@ from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.history_aware_retriever import create_history_aware_retriever
 from langchain.chains import create_retrieval_chain  # Corrected import
-from langchain_community.vectorstores import Qdrant
 import os
 
 
